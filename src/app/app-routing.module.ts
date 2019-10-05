08e2362776267dbbe3a4fbcf45a32ea399d59cf3
trandeng <32092408+trandeng@users.noreply.github.com>
2019-10-05 20:47:17 +0800

    add antd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RadarComponent } from './routes/radar/radar.component';
+import { ForecastComponent } from './routes/forecast/forecast.component';
+import { RealComponent } from './routes/real/real.component';
+import { AppComponent } from './app.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',
+  component: AppComponent,
+  children: [
+  {path: '',redirectTo: 'real',pathMatch: 'full'},
+  {path: 'forecast',component: ForecastComponent,data:{title: '预报天气'}},
+  {path: 'radar',component: RadarComponent,data:{title: '雷达图'}},
+  {path: 'real',component: RealComponent,data:{title: '实况天气'}}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/routes/real/real.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-real',
+  templateUrl: './real.component.html',
+  styleUrls: ['./real.component.less']
+})
+export class RealComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
